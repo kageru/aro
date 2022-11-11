97aa1611666a9b4680da49985e8f13bf40fecd4d
kageru <kageru@encode.moe>
2022-11-11 15:00:07 +0100

    remove tuirealm for tui

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
-use std::time::Duration;
-
+use crossterm::terminal::enable_raw_mode;
 use serde::Deserialize;
-use tui_realm_stdlib::List;
-use tuirealm::{
-    event::{Key, KeyEvent},
-    props::TextSpan,
-    terminal::TerminalBridge,
-    tui::layout::{Constraint, Layout},
-    Application, Component, Event, EventListenerCfg, MockComponent, NoUserEvent, PollStrategy,
-    Update,
+use std::{io, iter, time::Duration};
+use tui::{
+    backend::CrosstermBackend,
+    layout,
+    style::{Color, Style},
+    text::Text,
+    widgets::{Block, Borders, List, ListItem, Paragraph, Widget},
 };
 
-fn main() {
-    let mut app: Application<Id, Msg, NoUserEvent> = Application::init(
-        EventListenerCfg::default()
-            .default_input_listener(Duration::from_millis(20))
-            .poll_timeout(Duration::from_millis(10)),
-    );
-    app.mount(Id::MainList, Box::new(MyList {
-        cards: vec![
+type Terminal = tui::Terminal<CrosstermBackend<io::Stdout>>;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let stdout = io::stdout();
+    enable_raw_mode()?;
+    let mut backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.hide_cursor()?;
+    terminal.clear()?;
+    let all_cards= vec![
         Card::SpellCard {
             name: "The Cheerful Coffin".to_owned(),
             text: "Discard up to 3 Monster Cards from your hand to the Graveyard.".to_owned()
@@ -32,145 +32,47 @@ fn main() {
             r#type: "Zombie".to_owned(),
             attribute: "EARTH".to_owned(),
         }
-        ],
-        ..Default::default()
-    }), vec![])
-        .unwrap();
-    app.active(&Id::MainList).unwrap();
-    let model = Model {
-        app,
-        quit: false,
-        redraw: true,
-        terminal: TerminalBridge::new().unwrap(),
-        state: State::default(),
-    };
-    model.terminal.enter_alternate_screen();
-    model.terminal.enable_raw_mode();
+        ];
+    let mut cards = all_cards.clone();
+    let mut search_text = String::new();
 
-    while !model.quit {
-        match app.tick(PollStrategy::Once) {
-            Ok(messages) if messages.len() > 0 => {
-                model.redraw = true;
-                for msg in messages.into_iter() {
-                    model.update(Some(msg));
-                }
-            }
-            _ => {}
-        }
-        if model.redraw {
-            model.view(&mut app);
-            model.redraw = false;
-        }
+    loop {
+        refresh(&mut terminal, &cards, &search_text);
     }
-
-    model.terminal.leave_alternate_screen();
-    model.terminal.disable_raw_mode();
-    model.terminal.clear_screen();
+    Ok(())
 }
 
-#[derive(Debug, PartialEq)]
-enum Msg {
-    Quit,
-    SearchInput(char),
-    SearchClear,
-    Up,
-    Down,
-    Increment,
-    Decrement,
+fn refresh(term: &mut Terminal, cards: &Vec<Card>, search_text: &str) -> Result<(), io::Error> {
+    let mut list = selectable_list(
+        search_text,
+        cards
+            .iter()
+            .map(|c| ListItem::new(format!("{c:?}")))
+            .collect::<Vec<_>>(),
+        None,
+    );
+    term.draw(|mut frame| {
+        frame.render_widget(list, frame.size());
+    })?;
+    Ok(())
 }
 
-#[derive(Debug, PartialEq, Hash, Clone, Copy, Eq)]
-enum Id {
-    MainList,
-    SearchField,
+pub fn selectable_list<'a>(
+    title: &'a str,
+    content: Vec<ListItem<'a>>,
+    selected: Option<usize>,
+) -> List<'a> {
+    List::new(content)
+        .block(
+            Block::default()
+                .title(title)
+                .borders(Borders::TOP | Borders::RIGHT | Borders::LEFT),
+        )
+        .highlight_style(Style::default().fg(Color::LightGreen))
+        .highlight_symbol(">")
 }
 
-#[derive(MockComponent, Default)]
-struct MyList {
-    component: List,
-    cards: Vec<Card>,
-}
-
-impl Component<Msg, NoUserEvent> for MyList {
-    fn on(&mut self, ev: tuirealm::Event<NoUserEvent>) -> Option<Msg> {
-        match ev {
-            Event::Keyboard(KeyEvent {
-                code: Key::Char(ch),
-                ..
-            }) => match ch {
-                '+' => Some(Msg::Increment),
-                '-' => Some(Msg::Decrement),
-                ch => {
-                    self.component = self.component
-                        .rows(
-                        self.cards
-                            .iter()
-                            .map(|c| {
-                                vec![TextSpan::new(match c {
-                                    Card::SpellCard { name, .. } => name,
-                                    Card::EffectMonster { name, .. } => name,
-                                })]
-                            })
-                            .collect(),
-                    );
-                    Some(Msg::SearchInput(ch))
-                }
-            },
-            Event::Keyboard(KeyEvent { code: Key::Esc, .. }) => Some(Msg::Quit),
-            Event::Keyboard(KeyEvent {
-                code: Key::Backspace,
-                ..
-            }) => Some(Msg::SearchClear),
-            _ => None,
-        }
-    }
-}
-
-struct Model {
-    state: State,
-    app: Application<Id, Msg, NoUserEvent>,
-    terminal: TerminalBridge,
-    quit: bool,
-    redraw: bool,
-}
-
-impl Model {
-    pub fn view(&mut self, app: &mut Application<Id, Msg, NoUserEvent>) {
-        self.terminal
-            .raw_mut()
-            .draw(|f| {
-                let chunks = Layout::default()
-                    .constraints([Constraint::Percentage(100)])
-                    .split(f.size());
-                app.view(&Id::MainList, f, chunks[0]);
-            })
-            .unwrap();
-    }
-}
-
-impl Update<Msg> for Model {
-    fn update(&mut self, msg: Option<Msg>) -> Option<Msg> {
-        self.redraw = true;
-        match msg {
-            Some(Msg::SearchInput(c)) => self.state.filter.push(c),
-            Some(Msg::SearchClear) => self.state.filter.clear(),
-            Some(Msg::Quit) => self.quit = true,
-            _ => (),
-        };
-        None
-    }
-}
-
-#[derive(PartialEq, Eq, Default, Debug)]
-struct State {
-    filter: String,
-}
-
-struct ApiResponse {
-    data: Vec<Card>,
-}
-
-#[derive(Debug, Deserialize, PartialEq, Eq)]
+#[derive(Debug, Deserialize, PartialEq, Eq, Clone)]
 #[serde(tag = "type")]
 enum Card {
     #[serde(rename = "Spell Card")]
