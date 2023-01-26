91a56a77dc13499b1993d8524121e09fd6420d01
kageru <kageru@encode.moe>
2023-01-26 15:04:39 +0100

    deserlize all cards

--- src/main.rs
+++ src/main.rs
@@ -1,149 +1,172 @@
-use crossterm::terminal::enable_raw_mode;
-use serde::Deserialize;
-use std::{io, iter, time::Duration};
-use tui::{
-    backend::CrosstermBackend,
-    layout,
-    style::{Color, Style},
-    text::Text,
-    widgets::{Block, Borders, List, ListItem, Paragraph, Widget},
-};
-
-type Terminal = tui::Terminal<CrosstermBackend<io::Stdout>>;
+use serde::{de::Visitor, Deserialize, Deserializer};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let stdout = io::stdout();
-    enable_raw_mode()?;
-    let mut backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-    terminal.hide_cursor()?;
-    terminal.clear()?;
-    let all_cards= vec![
-        Card::SpellCard {
-            name: "The Cheerful Coffin".to_owned(),
-            text: "Discard up to 3 Monster Cards from your hand to the Graveyard.".to_owned()
-        },
-        Card::EffectMonster {
-            name: "Des Lacooda".to_owned(),
-            effect: "Once per turn: You can change this card to face-down Defense Position. When this card is Flip Summoned: Draw 1 card.".to_owned(),
-            atk: 500,
-            def: 600,
-            level: 3,
-            r#type: "Zombie".to_owned(),
-            attribute: "EARTH".to_owned(),
+    let cards: CardInfo =
+        serde_json::from_reader(std::io::BufReader::new(std::fs::File::open("cards.json")?))?;
+    println!("{} cards read", cards.data.len());
+    Ok(())
+}
+
+#[derive(Debug, Deserialize, PartialEq, Eq, Clone)]
+struct CardInfo {
+    data: Vec<Card>,
+}
+
+#[derive(Debug, Deserialize, PartialEq, Eq, Clone)]
+struct CardBase {
+    //#[serde(rename = "type", deserialize_with = "split_types")]
+    //card_type: Vec<String>,
+    name: String,
+    #[serde(rename = "desc")]
+    text: String,
+}
+
+fn split_types<'de, D: Deserializer<'de>>(deserializer: D) -> Result<Vec<String>, D::Error> {
+    struct SplittingVisitor;
+
+    impl<'de> Visitor<'de> for SplittingVisitor {
+        type Value = Vec<String>;
+
+        fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+            formatter.write_str("a string")
         }
-        ];
-    let mut cards = all_cards.clone();
-    let mut search_text = String::new();
 
-    loop {
-        refresh(&mut terminal, &cards, &search_text);
+        fn visit_str<E: serde::de::Error>(self, v: &str) -> Result<Self::Value, E> {
+            Ok(v.split_whitespace()
+                .filter(|t| t != &"Card")
+                .map(str::to_owned)
+                .collect())
+        }
     }
-    Ok(())
+    deserializer.deserialize_any(SplittingVisitor)
 }
 
-fn refresh(term: &mut Terminal, cards: &Vec<Card>, search_text: &str) -> Result<(), io::Error> {
-    let mut list = selectable_list(
-        search_text,
-        cards
-            .iter()
-            .map(|c| ListItem::new(format!("{c:?}")))
-            .collect::<Vec<_>>(),
-        None,
-    );
-    term.draw(|mut frame| {
-        frame.render_widget(list, frame.size());
-    })?;
-    Ok(())
-}
-
-pub fn selectable_list<'a>(
-    title: &'a str,
-    content: Vec<ListItem<'a>>,
-    selected: Option<usize>,
-) -> List<'a> {
-    List::new(content)
-        .block(
-            Block::default()
-                .title(title)
-                .borders(Borders::TOP | Borders::RIGHT | Borders::LEFT),
-        )
-        .highlight_style(Style::default().fg(Color::LightGreen))
-        .highlight_symbol(">")
+#[derive(Debug, Deserialize, PartialEq, Eq, Clone)]
+struct Monster {
+    // None for ?
+    atk: Option<i32>,
+    attribute: String,
+    #[serde(rename = "race")]
+    r#type: String,
+    // None for ? or link monsters
+    def: Option<i32>,
+    // also includes rank
+    level: Option<u8>,
 }
 
 #[derive(Debug, Deserialize, PartialEq, Eq, Clone)]
 #[serde(tag = "type")]
 enum Card {
-    #[serde(rename = "Spell Card")]
-    SpellCard {
-        name: String,
-        #[serde(rename = "desc")]
-        text: String,
+    #[serde(alias = "Spell Card", alias = "Trap Card")]
+    Backrow {
+        #[serde(flatten)]
+        base: CardBase,
+    },
+    #[serde(rename = "Skill Card")]
+    Skill {
+        #[serde(flatten)]
+        base: CardBase,
     },
     #[serde(
-        rename = "Effect Monster",
+        alias = "Effect Monster",
         alias = "Flip Effect Monster",
-        alias = "Union Effect Monster"
+        alias = "Fusion Monster",
+        alias = "Gemini Monster",
+        alias = "Link Monster",
+        alias = "Normal Monster",
+        alias = "Normal Tuner Monster",
+        alias = "Pendulum Effect Fusion Monster",
+        alias = "Pendulum Effect Monster",
+        alias = "Pendulum Effect Ritual Monster",
+        alias = "Pendulum Flip Effect Monster",
+        alias = "Pendulum Normal Monster",
+        alias = "Pendulum Tuner Effect Monster",
+        alias = "Ritual Effect Monster",
+        alias = "Ritual Monster",
+        alias = "Spirit Monster",
+        alias = "Synchro Monster",
+        alias = "Synchro Pendulum Effect Monster",
+        alias = "Synchro Tuner Monster",
+        alias = "Token",
+        alias = "Toon Monster",
+        alias = "Tuner Monster",
+        alias = "Union Effect Monster",
+        alias = "XYZ Monster",
+        alias = "XYZ Pendulum Effect Monster"
     )]
-    EffectMonster {
-        name: String,
-        #[serde(rename = "desc")]
-        effect: String,
-        atk: i32,
-        def: i32,
-        level: i32,
-        attribute: String,
-        #[serde(rename = "race")]
-        r#type: String,
+    Monster {
+        #[serde(flatten)]
+        base: CardBase,
+        #[serde(flatten)]
+        monster: Monster,
+        #[serde(default, rename = "linkval")]
+        link_rating: u8,
+        #[serde(default)]
+        linkmarkers: Vec<String>,
     },
 }
 
-#[test]
-fn test_spell() {
-    let s = r#"
-    {
-      "id": 41142615,
-      "name": "The Cheerful Coffin",
-      "type": "Spell Card",
-      "desc": "Discard up to 3 Monster Cards from your hand to the Graveyard.",
-      "race": "Normal"
-    }"#;
-    let coffin: Card = serde_json::from_str(s).unwrap();
-    assert_eq!(
-        coffin,
-        Card::SpellCard {
-            name: "The Cheerful Coffin".to_owned(),
-            text: "Discard up to 3 Monster Cards from your hand to the Graveyard.".to_owned()
-        }
-    )
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-#[test]
-fn test_monster() {
-    let s = r#"
-    {
-       "id": 2326738,
-       "name": "Des Lacooda",
-       "type": "Effect Monster",
-       "desc": "Once per turn: You can change this card to face-down Defense Position. When this card is Flip Summoned: Draw 1 card.",
-       "atk": 500,
-       "def": 600,
-       "level": 3,
-       "race": "Zombie",
-       "attribute": "EARTH"
-    }"#;
-    let munch: Card = serde_json::from_str(s).unwrap();
-    assert_eq!(
-        munch,
-        Card::EffectMonster {
-            name: "Des Lacooda".to_owned(),
-            effect: "Once per turn: You can change this card to face-down Defense Position. When this card is Flip Summoned: Draw 1 card.".to_owned(),
-            atk: 500,
-            def: 600,
-            level: 3,
-            r#type: "Zombie".to_owned(),
-            attribute: "EARTH".to_owned(),
-        }
-    )
+    #[test]
+    fn test_spell() {
+        let s = r#"
+        {
+          "id": 41142615,
+          "name": "The Cheerful Coffin",
+          "type": "Spell Card",
+          "desc": "Discard up to 3 Monster Cards from your hand to the Graveyard.",
+          "race": "Normal"
+        }"#;
+        let coffin: Card = serde_json::from_str(s).unwrap();
+        assert_eq!(
+            coffin,
+            Card::Backrow {
+                base: CardBase {
+                    card_type: vec!["Spell".to_owned()],
+                    name: "The Cheerful Coffin".to_owned(),
+                    text: "Discard up to 3 Monster Cards from your hand to the Graveyard."
+                        .to_owned()
+                }
+            }
+        )
+    }
+
+    #[test]
+    fn test_monster() {
+        let s = r#"
+        {
+           "id": 2326738,
+           "name": "Des Lacooda",
+           "type": "Effect Monster",
+           "desc": "Once per turn: You can change this card to face-down Defense Position. When this card is Flip Summoned: Draw 1 card.",
+           "atk": 500,
+           "def": 600,
+           "level": 3,
+           "race": "Zombie",
+           "attribute": "EARTH"
+        }"#;
+        let munch: Card = serde_json::from_str(s).unwrap();
+        assert_eq!(
+            munch,
+            Card::Monster {
+                base: CardBase {
+                    card_type: vec!["Effect".to_owned(), "Monster".to_owned()],
+                    name: "Des Lacooda".to_owned(),
+                    text: "Once per turn: You can change this card to face-down Defense Position. When this card is Flip Summoned: Draw 1 card.".to_owned(),
+                },
+                monster: Monster {
+                    atk: Some(500),
+                    def: Some(600),
+                    level: Some(3),
+                    r#type: "Zombie".to_owned(),
+                    attribute: "EARTH".to_owned(),
+                },
+                link_rating: 0,
+                linkmarkers: vec![]
+            },
+        )
+    }
 }
